28fa3b6ed6a77fc3e0a06caa3b16c53fa8fcfcae
[Pedro Sota] <psptaier@gmail.com>
2025-04-21 13:00:41 -0300

    delete temp files code runner

--- 04 Estructuras Repetitivas/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-num:int = int(input("Ingrese un número entero: "))
-num_invertido:int = 0
-
-while num > 0:
-  num_invertido = num_invertido * 10 + num % 10
-  num = num // 10
-
-print("h")
-print(num_invertido)

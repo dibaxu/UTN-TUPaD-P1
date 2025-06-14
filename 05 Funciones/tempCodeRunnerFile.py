36c5254a972cf6a1db7f576d28cd698c4432e8f9
[Pedro Sota] <psptaier@gmail.com>
2025-06-14 20:25:29 -0300

    recursividad

--- 05 Funciones/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-def calcular_promedio(a:int, b:int, c:int) -> float:
-    return (a + b + c) / 3
-
-a = int(input("Ingrese el primer número: "))
-b = int(input("Ingrese el segundo número: "))
-c = int(input("Ingrese el tercer número: "))
-promedio = calcular_promedio(a, b, c)
-print(f"El promedio de {a}, {b} y {c} es: {promedio}")
